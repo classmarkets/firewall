afa5ac0bbaab4ad30b17c5067aa3cd84f8597c08
Christian Fischer <sw-dev@computerlyrik.de>
2012-12-10 12:53:51 +0100

    added iptables support to firewall recipe

--- resources/rule.rb
+++ resources/rule.rb
@@ -32,21 +32,6 @@ attribute :logging, :kind_of => Symbol, :equal_to => [ :connections, :packets ]
 attribute :source, :regex => IP_CIDR_VALID_REGEX
 attribute :destination, :regex => IP_CIDR_VALID_REGEX
 attribute :dest_port, :kind_of => Integer
+attribute :dest_interface, :kind_of => String
 attribute :position, :kind_of => Integer
-
-def initialize(name, run_context=nil)
-  super
-  set_platform_default_providers
-  @action = :reject
-end
-
-private
-def set_platform_default_providers
-  [:ubuntu, :debian].each do |platform|
-    Chef::Platform.set(
-        :platform => platform,
-        :resource => :firewall_rule,
-        :provider => Chef::Provider::FirewallRuleUfw
-    )
-  end
-end
+attribute :stateful, :kind_of => String
